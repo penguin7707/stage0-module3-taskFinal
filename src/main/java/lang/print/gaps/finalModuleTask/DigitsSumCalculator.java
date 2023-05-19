c78f106594274b8bc749709a038a8acb7b169b1d
Stas <sizovstanislav10@gmail.com>
2023-05-19 15:28:31 +0700

    final

--- src/main/java/lang/print/gaps/finalModuleTask/DigitsSumCalculator.java
+++ src/main/java/lang/print/gaps/finalModuleTask/DigitsSumCalculator.java
@@ -2,5 +2,10 @@ package lang.print.gaps.finalModuleTask;
 
 public class DigitsSumCalculator {
     public void calculateSum(int number) {
+        int first = number/1000;
+        int second = (number-first)/100;
+        int third = (number-first-second)/10;
+        int fourth = (number-first-second)%10;
+        System.out.println(first+second+third+fourth);
     }
 }
--- src/main/java/lang/print/gaps/finalModuleTask/PrintingFalse.java
+++ src/main/java/lang/print/gaps/finalModuleTask/PrintingFalse.java
@@ -4,6 +4,7 @@ public class PrintingFalse {
     public static void main(String[] args) {
         int first = 9;
         int second = -17;
+        System.out.println(second>first);
         //write code here
     }
 }
